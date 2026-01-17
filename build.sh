f56bd9c59e85fe83579db3e0d63c6bf4b03ab8d8
APPLEPIE6969 <jimjolo2tt@gmail.com>
2026-01-17 20:17:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,17 +6,9 @@ set -o errexit
 pip install -r requirements.txt
 
 # 2. Install FFmpeg (Static Build)
-# Create a folder for ffmpeg
 mkdir -p ffmpeg_bin
-
-# Download the static build of FFmpeg
 echo "Downloading FFmpeg..."
 wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-
-# Extract it
 tar -xf ffmpeg-release-amd64-static.tar.xz -C ffmpeg_bin --strip-components 1
-
-# Clean up tar file
 rm ffmpeg-release-amd64-static.tar.xz
-
 echo "FFmpeg installed successfully."
